adb06fa58d539c582697129cf46ce0d8186334f0
Logika <dddd@>
2024-03-09 17:19:21 +0200

    init

--- main.py
+++ main.py
@@ -1,18 +1,86 @@
 from pygame import *
 
-window = display.set_mode((700, 500))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+
+class Enemy(GameSprite):
+    direction = "left"
+
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = "right"
+        if self.rect.x >= 700 - 85:
+            self.direction = "left"
+
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
+
+class Wall(sprite.Sprite):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
+        super().__init__()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.wall_x = wall_x
+        self.wall_y = wall_y
+        self.width = wall_width
+        self.height = wall_height
+        self.image = Surface((self.width, self.height))
+        self.image.fill((color_1, color_2, color_3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+win_width = 700
+win_height = 500
+
+mixer.init()
+mixer.music.load('sound/jungles.ogg')
+mixer.music.play()
+
+window = display.set_mode((win_width, win_height))
 display.set_caption("Доганялки")
+background = transform.scale(image.load("image/background.jpg"), (win_width, win_height))
 clock = time.Clock()
 
-background = transform.scale(image.load("background.png"), (700, 500))
-sprite1 = transform.scale(
-    image.load('sprite1.png'),
-    (100, 100)
-)
-sprite2 = transform.scale(
-    image.load('sprite2.png'),
-    (100, 100)
-)
+player = Player("image/hero.png", 5, win_height - 80, 4)
+monster = Enemy("image/cyborg.png", win_width - 120, 280, 2)
+final = GameSprite("image/treasure.png", win_width - 120, win_height - 80, 0)
+
+w1 = Wall(154, 205, 50, 100, 20, 450, 10)
+w2 = Wall(154, 205, 50, 100, 480, 350,10)
+w3 = Wall(154, 205, 50, 100, 20, 10, 380)
 
 x1 = 100
 x2 = 0
@@ -20,24 +88,42 @@ y1 = 100
 y2 = 0
 
 game = True
-while game:
-    window.blit(background, (0, 0))
-    window.blit(sprite1, (x1, y1))
-    window.blit(sprite2, (x2, y2))
+finish = False
 
+font.init()
+font = font.Font(None, 70)
+win = font.render('YOU WIN!', True, (255, 215, 0))
+lose = font.render('YOU LOSE!', True, (180, 0, 0))
+
+money = mixer.Sound('sound/money.ogg')
+kick = mixer.Sound('sound/kick.ogg')
+
+while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_LEFT] and x1 > 5:
-            x1 -= 10
-        if keys_pressed[K_RIGHT] and x1 < 595:
-            x1 += 10
-        if keys_pressed[K_DOWN] and y1 < 395:
-            y1 += 10
-        if keys_pressed[K_UP] and y1 > 5:
-            y1 -= 10
+    if not finish:
+        window.blit(background, (0, 0))
+        player.update()
+        monster.update()
+        player.reset()
+        monster.reset()
+        final.reset()
 
-    clock.tick(60)
+        w1.draw_wall()
+        w2.draw_wall()
+        w3.draw_wall()
+
+    if sprite.collide_rect(player, monster) or sprite.collide_rect(player, w1) or sprite.collide_rect(player, w2) or sprite.collide_rect(player, w3):
+        finish = True
+        window.blit(lose, (200, 200))
+        kick.play()
+
+    if sprite.collide_rect(player, final):
+        finish = True
+        window.blit(win, (200, 200))
+        money.play()
+
     display.update()
+    clock.tick(60)
